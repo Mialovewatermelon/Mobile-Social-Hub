90788bda239eb0976111c8cfe036725d3fbf5af1
Mialovewatermelon <46467032+Mialovewatermelon@users.noreply.github.com>
2020-10-26 13:43:04 +1100

    Trying Firebase
    Connection Failed

--- app/src/main/java/com/example/mobilesocialhub/MainActivity.java
+++ app/src/main/java/com/example/mobilesocialhub/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.mobilesocialhub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    String TAG = "FirebaseCheck";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database= FirebaseDatabase.getInstance();
+        Log.d(TAG,"database");
+        myRef= database.getReference("mobile-social-hub/message");
+        myRef.setValue("Hello, Wor");
+        DatabaseReference myRef2=database.getReference("messag2");
+        myRef2.setValue("No mistake");
+
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "key is: " + dataSnapshot.getKey());
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "key is: " + dataSnapshot.getKey());
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
